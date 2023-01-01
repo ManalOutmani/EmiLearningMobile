055011dd24cd75e2d58a33e03be240a6d41c60fa
Bash-Code <bachartrv2000@gmail.com>
2023-01-01 21:27:49 +0100

    login and subjects

--- app/src/main/java/com/example/cengonline/login/LoginActivity.java
+++ app/src/main/java/com/example/cengonline/login/LoginActivity.java
@@ -3,6 +3,7 @@ package com.example.cengonline.login;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -30,6 +31,7 @@ public class LoginActivity extends AppCompatActivity {
     Button btnLogin;
     TextView register;
     FirebaseAuth auth;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
